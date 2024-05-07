f3ab8dc4209dc82b68b87b55a4571712fff99d5d
Kedar Kulkarni <kulkarnikk07@gmail.com>
2024-05-06 19:34:57 -0700

    Added GPAIII, Shortest distance

--- Sql6 Combined.sql
+++ Sql6 Combined.sql
@@ -12,21 +12,23 @@ where rnk = 1
 
 -- 2 Problem 2 : Game Play Analysis III		(https://leetcode.com/problems/game-play-analysis-iii/)
 
-
+select player_id, event_date, sum(games_played) over (partition by player_id order by event_date) as 'games_played_so_far' 
+from Activity
 
 -- 3 Problem 3 : Shortest Distance in a Plane		(https://leetcode.com/problems/shortest-distance-in-a-plane/)
 
-
-
+select min(round(sqrt(pow(p2.x-p1.x,2)+ pow(p2.y-p1.y,2)),2)) as 'shortest' 
+from Point2D p1, Point2D p2
+where p1.x != p2.x
+or p1.y != p2.y
 
 -- 4 Problem 4 : Combine Two Tables	(https://leetcode.com/problems/combine-two-tables/)
-
-
-
-
--- 5 Problem 5 : Customers with Strictly Increasing Purchases		(https://leetcode.com/problems/customers-with-strictly-increasing-purchases/)
 
 select p.firstName, p.lastName, a.city, a.state
 from person p
 left join address a
 on p.personId = a.personId
+
+
+-- 5 Problem 5 : Customers with Strictly Increasing Purchases		(https://leetcode.com/problems/customers-with-strictly-increasing-purchases/)
+
